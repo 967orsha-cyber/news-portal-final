a78b9ac313e77aaec4d1e5b78a2705f47c3ce23d
Name <email@example.ru>
2026-02-12 15:00:50 +0300

    Add django-filter, fix project structure

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import NewsList, NewsDetail
+from .views import NewsList, NewsDetail, NewsSearch, NewsCreate, NewsUpdate, NewsDelete
+from .views import ArticleList, ArticleDetail, ArticleCreate, ArticleUpdate, ArticleDelete
 
 urlpatterns = [
+    # Статьи
+    path('articles/', ArticleList.as_view(), name='articles_list'),
+    path('articles/<int:pk>/', ArticleDetail.as_view(), name='article_detail'),
+    path('articles/create/', ArticleCreate.as_view(), name='article_create'),
+    path('articles/<int:pk>/edit/', ArticleUpdate.as_view(), name='article_edit'),
+    path('articles/<int:pk>/delete/', ArticleDelete.as_view(), name='article_delete'),
+    
+    # Новости
     path('', NewsList.as_view(), name='news_list'),
     path('<int:pk>/', NewsDetail.as_view(), name='news_detail'),
+    path('search/', NewsSearch.as_view(), name='news_search'),
+    path('create/', NewsCreate.as_view(), name='news_create'),
+    path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_edit'),
+    path('<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,16 +1,106 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django_filters.views import FilterView
 from .models import Post
+from .filters import PostFilter
+from .forms import NewsForm
+
+class NewsUpdate(UpdateView):
+    model = Post
+    form_class = NewsForm
+    template_name = 'news/news_create.html'  
+    success_url = reverse_lazy('news_list')
+    
+    def form_valid(self, form):
+        form.instance.post_type = 'NW'  
+        return super().form_valid(form)
+    
+class ArticleUpdate(UpdateView):
+    model = Post
+    form_class = NewsForm
+    template_name = 'news/article_create.html'  
+    success_url = reverse_lazy('articles_list')
+    
+    def form_valid(self, form):
+        form.instance.post_type = 'AR'  
+        return super().form_valid(form)
+    
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news/news_delete.html'
+    success_url = reverse_lazy('news_list')
+    
+    def get_queryset(self):
+        return Post.objects.filter(post_type=Post.NEWS)
+    
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'news/article_delete.html'
+    success_url = reverse_lazy('articles_list')
+    
+    def get_queryset(self):
+        return Post.objects.filter(post_type=Post.ARTICLE)
+
+class NewsCreate(CreateView):
+    model = Post
+    form_class = NewsForm
+    template_name = 'news/news_create.html'
+    success_url = reverse_lazy('news_list')
+    
+    def form_valid(self, form):
+        form.instance.post_type = 'NW'
+        # ВРЕМЕННО: берём первого автора
+        from .models import Author
+        form.instance.author = Author.objects.first()
+        return super().form_valid(form)
+    
+class ArticleCreate(CreateView):
+    model = Post
+    form_class = NewsForm
+    template_name = 'news/article_create.html'
+    success_url = reverse_lazy('articles_list')
+    
+    def form_valid(self, form):
+        form.instance.post_type = 'AR'
+        # ВРЕМЕННО: берём первого автора
+        from .models import Author
+        form.instance.author = Author.objects.first()
+        return super().form_valid(form)
+
+class NewsSearch(FilterView):
+    model = Post
+    template_name = 'news/news_search.html'
+    context_object_name = 'news_list'
+    filterset_class = PostFilter
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return Post.objects.filter(post_type=Post.NEWS).order_by('-created_at')
 
 class NewsList(ListView):
     model = Post
     template_name = 'news/news_list.html'
     context_object_name = 'news_list'
-    ordering = ['-created_at']
+    paginate_by = 10
     
     def get_queryset(self):
         return Post.objects.filter(post_type=Post.NEWS).order_by('-created_at')
+    
+class ArticleList(ListView):
+    model = Post
+    template_name = 'news/article_list.html'
+    context_object_name = 'articles_list'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return Post.objects.filter(post_type=Post.ARTICLE).order_by('-created_at')
 
 class NewsDetail(DetailView):
     model = Post
     template_name = 'news/news_detail.html'
     context_object_name = 'news'
+
+class ArticleDetail(DetailView):
+    model = Post
+    template_name = 'news/article_detail.html'
+    context_object_name = 'article'
